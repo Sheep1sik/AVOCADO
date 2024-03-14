c538fca24b2ff9dbf590ad5cf1a0b2e351253f0b
Sheep1sik <bohg6799@naver.com>
2024-03-14 17:21:03 +0900

    Feat: setup

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by 양원식 on 2024/03/14.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
